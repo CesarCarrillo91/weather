56451e932e94cdc43db752465c4f8f35c50a5b6b
carrillo455 <sistemas.carrillo01@gmail.com>
2015-11-30 15:26:28 -0600

    Add flex box and the js for feeding the weather info

--- js/main.js
+++ js/main.js
@@ -1,11 +1,55 @@
 // Docs at http://simpleweatherjs.com
 $(document).ready(function() {
+    var date = new Date();
+    var dias = ["DOM", "LUN", "MAR", "MIE", "JUE", "VIE", "SAB"];
+    var meses = ["ENE", "FEB", "MAR", "ABR", "MAY", "JUN",
+        "JUL", "AGO", "SEP", "OCT", "NOV", "DIC"];
+    var pad = function(str, max) {
+        str = str.toString();
+        return str.length < max ? pad("0" + str, max) : str;
+    }
+
     $.simpleWeather({
         location: 'Ciudad Madero, Tamaulipas, MX',
         woeid: '',
         unit: 'c',
         success: function(weather) {
+            // date.getDate() devuelve de 0 a 6; 0 es Domingo.
+            var dia_str = dias[date.getDay()];
+            var dia_num = pad(date.getDate(), 2);
+
+            // date.getMonth() devuelve de 0 a 11; 0 es Enero.
+            var mes = meses[date.getMonth()];
+
+            var temporada = date.getFullYear().toString().slice(2);
+
+            var gmt = -2;
+            if (date.getHours() > 11) {
+                var hora = pad(Math.abs(date.getHours() - 10 + gmt),2);
+                var minutos = pad(date.getMinutes(),2);
+                var meridiano = "PM";
+            } else {
+                var hora = pad(Math.abs(date.getHours() + 1 + gmt),2);
+                var minutos = pad(date.getMinutes(),2);
+                var meridiano = "AM";
+            }
+
+            var fecha = dia_str + " " + dia_num + " " + mes +
+                " " + temporada + " " + hora + ":" + minutos + " " + meridiano;
+
+            $("#header-title").html(fecha);
+
             $("#today-condition").addClass("icon-"+weather.code);
+            $("#today-temperature").html(weather.temp+"&deg;"+weather.units.temp);
+
+            $("#tomorrow-condition").addClass("icon-"+weather.forecast[1].code);
+            $("#tomorrow-temperature").html(weather.forecast[1].low+"&deg;"+weather.units.temp);
+
+            $("#after-tomorrow-condition").addClass("icon-"+weather.forecast[2].code);
+            $("#after-tomorrow-temperature").html(weather.forecast[2].low+"&deg;"+weather.units.temp);
+
+            $("#last-day-condition").addClass("icon-"+weather.forecast[3].code);
+            $("#last-day-temperature").html(weather.forecast[3].low+"&deg;"+weather.units.temp);
             // var html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
             // html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
             // html += '<li class="currently">'+weather.currently+'</li>';
